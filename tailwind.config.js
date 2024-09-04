adc6aeebb2a2203756e88220e4d6db0eef8f6908
Damian Pietrzak <150837797+pr3ston1989@users.noreply.github.com>
2024-09-04 23:43:22 +0200

    Add hero, main and bottom section.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "roboto-flex": ['"Roboto Flex"', "sans-serif"],
+        "roboto-condensed": ["roboto_condensedregular", "sans-serif"],
+        bebas: ["bebas_neueregular", "sans-serif"],
+      },
+      fontSize: {
+        "15px": "15px",
+      },
+      lineHeight: {
+        22.5: "22.5px",
+      },
+      letterSpacing: {
+        "tight-custom-02": "-0.02em",
+        "tight-custon-04": "-0.04em",
+      },
+      maxWidth: {
+        1440: "1440px",
+        1200: "1200px",
+      },
+    },
   },
   plugins: [],
-}
+};
