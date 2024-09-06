7079889c39bd1ef79e59dbb74619f57816a7836a
Damian Pietrzak <150837797+pr3ston1989@users.noreply.github.com>
2024-09-06 18:26:31 +0200

    Add swiper gallery.

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,8 +15,8 @@ module.exports = {
         22.5: "22.5px",
       },
       letterSpacing: {
-        "tight-custom-02": "-0.005em",
-        "tight-custom-03": "-0.03em",
+        "tight-custom-02": "-0.006em",
+        "tight-custom-03": "-0.036em",
         "tight-custon-04": "-0.038em",
       },
       maxWidth: {
@@ -25,5 +25,26 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents, theme }) {
+      addComponents({
+        ".line-clamp-brackets": {
+          display: "-webkit-box",
+          "-webkit-line-clamp": "2",
+          "-webkit-box-orient": "vertical",
+          overflow: "hidden",
+          position: "relative",
+        },
+        ".line-clamp-brackets::after": {
+          content: '"[ ... ]"',
+          position: "absolute",
+          bottom: "0",
+          right: "0",
+          background: theme("colors.gray.900"),
+          paddingLeft: "0.25rem",
+          paddingRight: "0.25rem",
+        },
+      });
+    },
+  ],
 };
